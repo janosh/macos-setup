af088387644f0df7b5a6574a672bd45563a9bedc
janosh <janosh.riebesell@gmail.com>
2019-06-27 12:02:16 +0100

    .eslintrc.js add react-hooks plugin and update docstring

--- dotfiles/.eslintrc.js
+++ dotfiles/.eslintrc.js
@@ -1,11 +1,10 @@
 /*
 #!/bin/sh
 #
-# Pre-commit hook called by "git commit" with no arguments.
+# Pre-commit hook called by 'git commit'.
 # Checks staged .js(x) files for eslint errors.
-# Exits with non-zero status after issuing an appropriate message if
-# it wants to stop the commit.
-# Place in {projectRoot}/.git/hooks/pre-commit
+# Exits with non-zero status after issuing an error if a commit is prevented.
+# To install, copy this script to {projectRoot}/.git/hooks/pre-commit
 
 git diff --diff-filter=d --cached --name-only | grep "\.js.\?$" | xargs ./node_modules/.bin/eslint
 */
@@ -25,7 +24,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react'],
+  plugins: ['react', 'react-hooks'],
   rules: {
     indent: ['error', 2, { SwitchCase: 1 }],
     quotes: ['error', 'backtick', { avoidEscape: true }],
@@ -33,6 +32,9 @@ module.exports = {
     'linebreak-style': ['error', 'unix'],
     'react/prop-types': 'off',
     'react/display-name': 'off',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
   settings: {
     react: {
