9d6cab483b4f729448f7c3f09ab35ae68cf118ef
Janosh Riebesell <janosh.riebesell@gmail.com>
2021-05-30 08:49:16 +0200

    add shellcheckpy and fix warnings

--- setup/1-setup.sh
+++ setup/1-setup.sh
@@ -1,3 +1,5 @@
+
+  sudo --stdin --validate <<< "${SUDO_PASSWORD}" 2> /dev/null
 }
 
 ask_details() {
--- setup/main.sh
+++ setup/main.sh
@@ -10,3 +10,4 @@ install() {
 }
 
 # Run and log errors to file (but still show them when they happen).
+readonly ERROR_LOG="${HOME}/Desktop/install_errors.log"
