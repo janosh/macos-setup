ee3b90f864da6a0498ab6d1e317abd234172019f
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-01-14 11:43:16 +0000

    add scripts/compress_screenshots.py and markdownlint pre-commit hook

---
+++ scripts/compress_screenshots.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import traceback
+
+# Use this script by creating a new folder action with 'Folder Actions Setup.app' that
+# watches '~/Desktop' (pass inputs as arguments!) and runs this shell script:
+# source ~/.zshrc # can be omitted if the system python3 is 3.9+
+
+
+# If folder action doesn't appear to trigger after setup, try toggling
+# "Enable Folder Actions" in "Folder Actions Setup.app" or add one of the
+# default actions and see if that runs.
+
+
+
+
+
+
+
+
