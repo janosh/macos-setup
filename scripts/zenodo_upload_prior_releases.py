01d42998f82885183840bc3389776dd7fb0e970b
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-01-02 20:10:52 -0800

    Add `scripts/zenodo_upload_prior_releases.py`
    add scripts/zenodo_upload_prior_releases.py

---
+++ scripts/zenodo_upload_prior_releases.py
@@ -0,0 +1,16 @@
+import requests
+
+__date__ = "2022-12-27"
+
+# adapted from script posted by @jrs65 in
+# https://github.com/zenodo/zenodo/issues/1463#issuecomment-1007602828
+
+
+headers = {"Accept": "application/vnd.github.v3+json"}
+
+
+
+print(f"prior {len(releases)=}")
+
+
+
