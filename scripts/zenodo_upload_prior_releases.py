0fdf51ffd415e8d3dc11473fa1b0e5c73d383f0b
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-07-20 06:49:15 -0700

    fix scripts/zenodo_upload_prior_releases.py 401 unauthorized
    due to quotes around access_token in post url

--- scripts/zenodo_upload_prior_releases.py
+++ scripts/zenodo_upload_prior_releases.py
@@ -5,9 +5,10 @@ __date__ = "2022-12-27"
 # adapted from script posted by @jrs65 in
 # https://github.com/zenodo/zenodo/issues/1463#issuecomment-1007602828
 
-repo = "janosh/pymatviz"
-# token copied from
-# https://github.com/janosh/pymatviz/settings/hooks/394259611
+repo = "janosh/matbench-discovery"
+# token appears to be the same for every repo and was copied from the end of the
+# payload url in the Zenodo webhook settings page:
+# https://github.com/janosh/pymatviz/settings/hooks/424898772
 access_token = "**************"
 
 headers = {"Accept": "application/vnd.github.v3+json"}
@@ -21,18 +22,23 @@ releases = requests.get(
 print(f"prior {len(releases)=}")
 
 
-# upload oldest release first
-for release in reversed(releases):
-
+# -- upload oldest release first --
+# for release in reversed(releases):
+# -- only upload oldest release --
+for release in [releases[-1]]:
     payload = dict(
         action="published",
         release=release,
         repository=repo_response.json(),
     )
 
-    submit_response = requests.post(
-        f"https://zenodo.org/api/hooks/receivers/github/events/?{access_token=}",
+    response = requests.post(
+        f"https://zenodo.org/api/hooks/receivers/github/events/?{access_token=!s}",
         json=payload,
     )
 
-    print(submit_response.status_code)
+    if response.status_code != 200:
+        msg = response.json()["message"]
+        raise ValueError(f"Failed to submit release to Zenodo.\n\tMessage: {msg}")
+    print(f"uploaded {release['tag_name']}")
+    print(response.json())
