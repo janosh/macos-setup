aa8f576a226d3e357786aa34ee04d59506478b09
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-02-19 08:20:51 +0100

    fix scripts/zenodo_upload_prior_releases.py raising on response.status_code == 202
    lots of new words in cSpell_dict.txt
    remove unused iTerm2 auto-setup from setup/3-config.sh

--- scripts/zenodo_upload_prior_releases.py
+++ scripts/zenodo_upload_prior_releases.py
@@ -5,6 +5,7 @@ __date__ = "2022-12-27"
 # adapted from script posted by @jrs65 in
 # https://github.com/zenodo/zenodo/issues/1463#issuecomment-1007602828
 
+repo = "materialsproject/atomate2"  # 2024-02-19
 
 headers = {"Accept": "application/vnd.github.v3+json"}
 
@@ -15,6 +16,9 @@ print(f"prior {len(releases)=}")
 
 # -- upload oldest release first --
 # for release in reversed(releases):
+# -- to only upload newest release, use releases[0] --
+for release in [releases[0]]:
+    response.raise_for_status()
 
     print(f"uploaded {release['tag_name']}")
     print(response.json())
